73faba6491fe8948c2a174e2683f8802fe985988
satyamwathrey <swathrey1@gmail.com>
2022-04-14 19:47:00 +0530

    Ability to find frequency of words in a given sentence

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message
+            Console.WriteLine("=-=-=-=-=-Welcome To The Hash Tables Program-=-=-=-=-=");
+
+            //Variable
+            int key = 0;
+
+            //Paragraph words assign to array
+            string paragraphPhrase = "To be or not to be";
+            string[] paragraph = paragraphPhrase.Split(' ');
+
+            //Object of class MyMapNode
+            MyMapNode<int, string> myMapNode = new MyMapNode<int, string>(paragraph.Length);
+
+            //Add 
+            foreach (string word in paragraph)
+            {
+                myMapNode.Add(key, word);
+                key++;
+            }
+
+            //Display
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("frequency of words ");
+            Console.WriteLine("--------------------------------");
+
+
+            myMapNode.Display();
+
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("--------------------------------");
+            Console.ReadLine();
+
+            Console.ReadLine();
+        }
+    }
+}
